1f5c917027b9bebe7dd207aebfd9f61986efb5a2
Enis  Emir <enissemir@gmail.com>
2018-12-02 20:17:43 +0300

    Entity Framework

---
+++ EntityFrameWorkSamples/EntityFrameWorkSamples/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkSamples
+{
+    public class Context : DbContext
+    {
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Urun> Urunler { get; set; }
+
+    }
+}
---
+++ EntityFrameWorkSamples/EntityFrameWorkSamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kategori k = new Kategori();
+            k.Id = 4;
+            k.KategoriAdi = "bigisayar";
+
+            Context db = new Context();
+            db.Kategoriler.Add(k);
+            db.SaveChanges();
+
+            Console.WriteLine("veri kayıt edildi");
+            Console.ReadLine();
+
+
+
+            //List<Kategori> Kategoriler = new List<Kategori>
+            //{
+            //    new Kategori(){Id=1, KategoriAdi= "telefon"},
+            //    new Kategori(){Id=1, KategoriAdi= "bilgisyar"},
+            //    new Kategori(){Id=1, KategoriAdi= "tablet"}
+            //};
+
+            //Kategoriler.Add(k);
+        }
+    }
+}
