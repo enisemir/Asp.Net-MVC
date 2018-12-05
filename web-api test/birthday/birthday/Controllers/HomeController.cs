35c868a31f94c4cd38a3ffef0429555fdda522ca
Enis Emir <enissemir@gmail.com>
2018-12-05 18:06:09 +0300

    web api

---
+++ web-api test/birthday/birthday/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using birthday.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace birthday.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DavetiyeFormu()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DavetiyeFormu(DavetiyeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Veritabani.Add(model);
+                return View("Thanks", model);
+            }
+            return View(model);
+        }
+        [ChildActionOnly]
+        public ActionResult Katilanlar()
+        {
+            var katilanlari = Veritabani.Liste.Where(i => i.KatilmaDurumu == true);
+            return PartialView(katilanlari);
+        }
+    }
+}
---
+++ web-api test/birthday/birthday/Models/DavetiyeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace birthday.Models
+{
+    public class DavetiyeModel
+    {
+        public string Ad { get; set; }
+        public string Eposta { get; set; }
+        public bool? KatilmaDurumu { get; set; }
+    }
+}
